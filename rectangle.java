ef76cbc98be17ed469a03a4bb0b7d37f382a3f0c
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-02-21 12:45:28 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,16 @@
+
+public class rectangle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int l,b,ar;
+		l=12;
+		b=25;
+		ar=(l*b);
+		System.out.println("length is "+l+"breadth is "+b+"area is " +ar);
+
+	}
+
+}
