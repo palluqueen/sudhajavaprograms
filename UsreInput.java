acc07db34ccc7dd2363a27abf35343b09d2adc76
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-02-21 12:53:27 +0530

    Add files via upload

---
+++ UsreInput.java
@@ -0,0 +1,29 @@
+package p1;
+
+import java.util.Scanner;
+
+public class UsreInput {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+	int a[]=new int[5];
+	int i;
+	System.out.println("enter a elements"+a.length);
+
+	for( i=0;i<=a.length;i++){
+	a[i]= sc.nextInt();
+	System.out.println("the array elements are"+a[i]);
+
+	
+	
+	
+
+	}
+	}
+}
+		
+
+
