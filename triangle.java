c708ef47232e6bb66c7a984217e12f154e751555
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-02-21 12:47:23 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,15 @@
+
+public class triangle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		float b,h,tr;
+		b=56;
+		h=25;
+		tr=1/2*(b*h);
+		System.out.println("area of triangle is"+tr);
+
+	}
+}
