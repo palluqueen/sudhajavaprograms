a2a917ba72a14cbfab230c47d8e1460c8210fd0e
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-02-21 12:48:47 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,17 @@
+package p1;
+
+public class ArrayDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a[]={1,2,3,4,};
+		System.out.println("first num"+a[0]);
+		System.out.println("second num"+a[1]);
+
+		
+
+	}
+
+}
