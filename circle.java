c7033eada9950a0533a5e1f889aaf63469986b79
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-02-21 12:46:34 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,18 @@
+
+public class circle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		float r,pi,ci; 
+		r=34;
+		pi=3.142f;
+		ci=(pi*(r*r));
+		System.out.println("area of circle is"+ci);
+		
+		
+
+	}
+
+}
