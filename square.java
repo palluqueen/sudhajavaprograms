d13f7d619cdd96ec53984915d582c06b149d1f3c
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-02-21 12:46:58 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,17 @@
+
+public class square {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		float n1,n2,sq;
+		n1=15.6f;
+		n2=3;
+		sq=n1*n2;
+	   System.out.println("one side "+n1+"anotherside is "+n2+"suare is "+sq);
+	   
+
+	}
+
+}
