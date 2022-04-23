cf1dd9a800c13bb0b24a18af8f4ed1d2cfe09abb
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-04-23 19:43:21 +0530

    Add files via upload

---
+++ mytree.java
@@ -0,0 +1,21 @@
+
+package p1;
+
+
+import java.util.Hashtable;
+import java.util.TreeMap;
+
+public class mytree {
+	public static void main(String[] args) {
+		TreeMap<Integer,String>   ob=new TreeMap <Integer,String> ();
+		ob.put(6, "sudha");
+		ob.put(7, "queen");
+		ob.put(8, "pavi");
+System.out.println(ob);
+		
+
+
+	}
+
+}
+
