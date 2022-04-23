1ecca06a235dca4bd6920820d0f83c3760c11b4f
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-04-23 19:42:24 +0530

    Add files via upload

---
+++ MyHasTable.java
@@ -0,0 +1,19 @@
+
+package p1;
+
+
+import java.util.Hashtable;
+
+public class MyHasTable {
+	public static void main(String[] args) {
+		Hashtable<Integer,String>   ob=new Hashtable<Integer,String> ();
+		ob.put(6, "sudha");
+		ob.put(7, "queen");
+		ob.put(8, "pavi");
+System.out.println(ob);
+		
+
+
+	}
+
+}
