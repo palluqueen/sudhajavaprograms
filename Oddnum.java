0d5ee9ea5874e17ed61519ea63228b4e68161b42
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-04-23 19:25:15 +0530

    Add files via upload

---
+++ Oddnum.java
@@ -0,0 +1,20 @@
+package p1;
+
+public class Oddnum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int i;
+		i=100;
+	   while(i<=100) {
+		   System.out.println("the num is "+(i=i+3));
+		  
+		   
+	   }
+	   
+
+	}
+
+}
