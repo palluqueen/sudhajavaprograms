37f6809b79bf9d35f20488d9933edf50f49c61a1
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-04-23 19:42:55 +0530

    Add files via upload

---
+++ MyLinked.java
@@ -0,0 +1,23 @@
+
+package p1;
+
+
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+
+public class MyLinked {
+	public static void main(String[] args) {
+		LinkedHashMap<Integer,String>   ob=new LinkedHashMap<Integer,String> ();
+		ob.put(6, "sudha");
+		ob.put(7, "queen");
+		ob.put(8, "pavi");
+		ob.put(9, "pavi");
+
+System.out.println(ob);
+		
+
+
+	}
+
+}
+
