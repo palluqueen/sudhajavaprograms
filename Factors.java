439511daa55d9f9abacaa819e00c23fd73552f5b
palluqueen <99879581+palluqueen@users.noreply.github.com>
2022-04-24 09:38:56 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,18 @@
+package p1;
+
+import java.util.Scanner;
+
+public class Factors {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	 int num,i;
+	Scanner sc=new Scanner(System.in);
+	 System.out.println("enter a number ");
+     num=sc.nextInt();
+    
+	}
+
+}
